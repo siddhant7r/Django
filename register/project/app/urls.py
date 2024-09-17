79cc61a1ce059d2483fc91b14747c8467d5ea86c
Siddhant <siddhant11raj@gmail.com>
2024-09-17 23:21:50 +0530

    register data

--- register/project/app/urls.py
+++ register/project/app/urls.py
@@ -1,5 +1,8 @@
 from app import views
 from django .urls import path
 urlpatterns=[
-    path('',views.siddhant,name='siddhant')
+    path('',views.siddhant,name='siddhant'),
+    path('register/',views.register,name='register'),
+    path('register/',views.register,name='register'),
+    path('userlogin/',views.userlogin,name='userlogin'),
 ]
--- register/project/app/views.py
+++ register/project/app/views.py
@@ -4,3 +4,26 @@ from django.shortcuts import render
 def siddhant(request):
     return render(request,'home.html')
 
+def register(request):
+    print(request.method)
+    print(request.POST)
+    # return render(request,'register.html')
+
+    cstoken=request.POST.get('csrfmiddlewaretoken')
+    name=request.POST.get('nm')
+    email=request.POST.get('em')
+    contact=request.POST.get('con')
+    password=request.POST.get('pass')
+
+    # print(name,email,contact,password)
+    response=render(request,'login.html')
+    response.set_cookie('name',name)
+    response.set_cookie('email',email)
+    response.set_cookie('contact',contact)
+    response.set_cookie('password',password)
+    return response
+
+def userlogin(request):
+    if request.method=='POST':
+        email=request.POST.get('email')
+        password=request.POST.get('password')
