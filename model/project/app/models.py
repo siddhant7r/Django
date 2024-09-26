b85df71e3857436e864b35308c7317f156e8039e
Siddhant <siddhant11raj@gmail.com>
2024-09-26 19:30:45 +0530

    added models

---
+++ model/project/app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Student(models.Model):
+    stu_name=models.CharField(max_length=50)
+    stu_email=models.EmailField()
+    stu_contact=models.IntegerField()
+    stu_password=models.CharField(max_length=25)
