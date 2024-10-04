0510caa2d1673abac23c211e320d25393ad08e5c
Siddhant <siddhant11raj@gmail.com>
2024-10-04 22:08:50 +0530

    Initial commit with .gitignore to ignore env folders

---
+++ filtertry/project/app/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    stu_name=models.CharField(max_length=50)
+    stu_email=models.EmailField()
+    stu_contact=models.IntegerField()
+    stu_password=models.CharField(max_length=25)
+
+def __str__(self):
+    return self.stu_name
+
+class Query(models.Model):
+    stu_name=models.CharField(max_length=50)
+    stu_email=models.EmailField()
+    stu_query=models.TextField(max_length=50)
+
+def __str__(self):
+    return self.stu_query   
---
+++ filtertry/project/app/urls.py
@@ -0,0 +1,11 @@
+from django .urls import path
+from app import views
+
+urlpatterns=[
+    path('',views.home,name='home'),
+    path('register/',views.register,name='register'),
+    path('login/',views.login,name='login'),
+    path('exclude/',views.exclude,name='exclude'),
+    path('query/',views.query,name='query'),
+
+]
